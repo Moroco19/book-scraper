923b0e3d89065a76900191749ee8f70f69f03bd5
Andrew Moroco <andrew.moroco@gmail.com>
2024-02-29 12:27:16 -0800

    updated page scraped to grab url of instock books

--- pageScraper.js
+++ pageScraper.js
@@ -4,6 +4,16 @@ const scraperObject = {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);
+
+        await page.waitForSelector('.page_inner');
+
+        let urls = await page.$$eval('section ol > li', links => {
+            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+            links = links.map(el => el.querySelector('h3 > a').href);
+
+            return links;
+        });
+        console.log(urls);
     }
 }
 
